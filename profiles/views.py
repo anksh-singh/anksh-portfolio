5b9f8f5ec587ff4c1eb658664f88dd348bc45d7c
ankitsingh1999 <ankitsi031999@gmail.com>
2023-03-06 12:00:00 +0000

    submitted email captured

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.mail import send_mail
+from .forms import EmailForm
 # from .forms import EmailForm
 
 # Create your views here.
@@ -16,14 +17,18 @@ def about(request):
 
 
 def submit_email(request):
-    return HttpResponse('Thank you for contacting us!')
-#     if request.method == 'POST':
-#         form = EmailForm(request.POST)
-#         if form.is_valid():
-#             email = form.cleaned_data['email']
-#             send_mail('New Email Submission', email, 'from@example.com', ['to@example.com'])
-#             return render(request, 'success.html')
-#     else:
-#         form = EmailForm()
+    # return HttpResponse('Thank you for contacting us!')
+    if request.method == 'POST':
+        form = EmailForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            print("email", email)
+
+
+            # send_mail('New Email Submission', email, 'from@example.com', ['to@example.com'])
+            # return render(request, 'success.html')
+    else:
+        form = EmailForm()
 
 #     return render(request, 'email_form.html', {'form': form})
+    return  HttpResponse("Thank you for contacting me!")
