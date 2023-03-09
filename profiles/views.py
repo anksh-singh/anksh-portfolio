e293495fbbe9826c485ce8f065517e9e80e0a0f8
ankitsingh1999 <ankitsi031999@gmail.com>
2023-03-10 00:14:56 +0530

    block content and endblock added in the index template

--- profiles/views.py
+++ profiles/views.py
@@ -15,14 +15,14 @@ def about(request):
     return render(request, 'about.html')
 
 
-def email_form(request):
-    if request.method == 'POST':
-        form = EmailForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data['email']
-            send_mail('New Email Submission', email, 'from@example.com', ['to@example.com'])
-            return render(request, 'success.html')
-    else:
-        form = EmailForm()
+# def email_form(request):
+#     if request.method == 'POST':
+#         form = EmailForm(request.POST)
+#         if form.is_valid():
+#             email = form.cleaned_data['email']
+#             send_mail('New Email Submission', email, 'from@example.com', ['to@example.com'])
+#             return render(request, 'success.html')
+#     else:
+#         form = EmailForm()
 
-    return render(request, 'email_form.html', {'form': form})
+#     return render(request, 'email_form.html', {'form': form})
