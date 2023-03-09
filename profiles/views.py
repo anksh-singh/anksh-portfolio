37b276d13cd0591130c8af041863b7128530acf2
ankitsingh1999 <ankitsi031999@gmail.com>
2023-03-10 00:11:16 +0530

    imported email sender modules

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.core.mail import send_mail
+# from .forms import EmailForm
 
 # Create your views here.
 
